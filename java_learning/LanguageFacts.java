ad21ecc8bfc9ffdb7a0b279a90c93262049f1843
renanbodart <braggio.bodart@gmail.com>
2022-12-08 15:53:06 -0300

    New topic lesson

---
+++ java_learning/LanguageFacts.java
@@ -0,0 +1,7 @@
+public class LanguageFacts {
+  public static void main(String[] args) {
+    System.out.println("Java is a class-based language.");
+
+    System.out.print("Learning Java is very cool!");
+  }
+}
