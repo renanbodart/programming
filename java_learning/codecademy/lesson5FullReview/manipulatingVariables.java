d6af652378aea822b987e5797a0803b799a71c3c
renanbodart <braggio.bodart@gmail.com>
2023-01-15 17:41:04 -0300

    new changes

--- java_learning/codecademy/lesson5FullReview/manipulatingVariables.java
+++ java_learning/codecademy/lesson5FullReview/manipulatingVariables.java
@@ -1,11 +1,19 @@
+package lesson5FullReview;
 public class manipulatingVariables {
   public static void main(String[] args){
-    /*Addition and Subtraction: To addition and subtraction operations use*/
+    //Addition and Subtraction operations
     double value1 = 10000;
     double value2 = 20000;
     double additionOperation = value1 + value2;
     double subtractionOperation = value2 - value1;
     System.out.println("The addition operation is " + additionOperation);
     System.out.println("The subtraction operation is " + subtractionOperation);
+    
+    //Multiplication and Division operations
+
+    double multiplicationOpValue1 = 4;
+    double multiplicationOpValue2 = 8;
+    double productMultiOp = multiplicationOpValue1 * multiplicationOpValue2;
+    System.out.println(productMultiOp);
   }
 }
