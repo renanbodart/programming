12daffa176ff33e8c1abbcea74d632a53a2ffddc
renanbodart <braggio.bodart@gmail.com>
2023-01-15 16:30:04 -0300

    new folder and lesson

---
+++ lesson5FullAndReview/manipulatingVariables.java
@@ -0,0 +1,11 @@
+public class manipulatingVariables {
+  public static void main(String[] args){
+    /*Addition and Subtraction: To addition and subtraction operations*/
+    double value1 = 10000;
+    double value2 = 20000;
+    double additionOperation = value1 + value2;
+    double subtractionOperation = value2 - value1;
+    System.out.println("The addition operation is " + additionOperation);
+    System.out.println("The subtraction operation is " + subtractionOperation);
+  }
+}
